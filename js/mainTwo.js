da9a62e225e0755f530e5eace798d37a1e60b0bf
Gerald Encabo <gerald_encabo@yahoo.com>
2021-06-01 12:54:25 -0400

    initial commit

--- js/mainTwo.js
+++ js/mainTwo.js
@@ -2,7 +2,7 @@
 const sections = document.querySelectorAll('section');
 const nav_active= document.querySelector('.nav_active');
 const options = {
-    threshold: 0.7
+    threshold: 0.5
 };
 
 let observer = new IntersectionObserver(navCheck, options);
